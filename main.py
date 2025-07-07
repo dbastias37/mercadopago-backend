73d6812ead066d060850008188e5ea53af1aa6df
dbastias37 <dbastias.alfaro@gmail.com>
2025-07-07 11:34:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ def pagar():
     return redirect(link_pago)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
